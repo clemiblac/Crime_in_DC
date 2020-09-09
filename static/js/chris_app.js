a77f75e9692a899b1c4d0a55e62c2fc9152b539a
unknown <cmikus6@gmail.com>
2020-09-09 17:53:27 -0400

    full choropleth functionality

--- static/js/chris_app.js
+++ static/js/chris_app.js
@@ -15,8 +15,6 @@ var myMap = L.map("myMap1", {
   
   // Use this link to get the geojson data.
   
-  
-  
   // Grabbing our GeoJSON data..
 //   d3.json(link).then(function(data) {
 //       console.log(data);
@@ -69,27 +67,24 @@ var dapurpleMarker = L.AwesomeMarkers.icon({
     markerColor: 'darkpurple'
 });
 
+var crimeLayer = new L.LayerGroup();
+var heatArray = new L.LayerGroup();
+var heat = [];
+var markers = [];
 
 d3.select('#submit').on('click', function(filters){
-    if(myMap != undefined){
-        myMap.remove();
+    if(crimeLayer){
+        console.log("crimeLayer detected");
+        myMap.removeLayer(crimeLayer);
+        crimeLayer.clearLayers();
+        markers = [];
     }
-    var myMap = L.map("myMap1", {
-        center: [38.8951, -77.0364],
-        zoom: 12
-      });
-      
-    // Adding tile layer
-    L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/streets-v11",
-    accessToken: API_KEY
-    }).addTo(myMap);
-    
+    if(heatArray){
+        myMap.removeLayer(heatArray);
+        heat = [];
+    }
     // Use this link to get the geojson data.
+    console.log('heat length ' + heat.length);
     var yearSelection = d3.select("#selectYear").node().value;
     var month = d3.select("#selectMonth").node().value;
     var offense = d3.select("#selectCrime").node().value;
@@ -101,17 +96,17 @@ d3.select('#submit').on('click', function(filters){
     d3.json(link).then(function(data) {
         console.log(data);
         console.log(data[0].REPORT_DAT.substring(4,8));
-        var heatArray = [];
         for (var i = 0; i < data.length; i++) {
             var loc = [data[i].Y, data[i].X];
             if (loc) {
-              heatArray.push(loc)
+              heat.push(loc)
             }
         }
-        var heat = L.heatLayer(heatArray, {
+        heatArray = L.heatLayer(heat, {
             radius: 12,
             blur: 20
         }).addTo(myMap);
+        console.log('heat length ' + heat.length);
         var data_month = data.filter(data => data.REPORT_DAT.substring(4,8) == month);
         var filtered = data_month.filter(element => element.OFFENSE == offense);
         console.log(filtered);
@@ -119,10 +114,15 @@ d3.select('#submit').on('click', function(filters){
             var location = [filtered[i].Y, filtered[i].X];
             console.log(location);
             if (location) {
-              var marker = L.marker(location, {icon: redMarker}).addTo(myMap);
+              markers.push(location)
             }
+        }
+        console.log(markers);
+        for (var i=0; i < markers.length; i++){
+            var marker = L.marker(markers[i], {icon: redMarker}).addTo(crimeLayer);
             marker.bindPopup("<h1>" + filtered[i].REPORT_DAT + "</h1> <hr> <h4>" + filtered[i].BLOCK + "</h4>");
-        };
+        }  
+        crimeLayer.addTo(myMap);
     });
 });
 
@@ -130,25 +130,29 @@ var grocery = "/grocery";
 var police = "/police";
 var pubschool = "/pubschool";
 var prischool = "/prischool";
+var policeLayer = new L.LayerGroup();
+var groceryLayer = new L.LayerGroup();
+var publicLayer = new L.LayerGroup();
+var privateLayer = new L.LayerGroup();
 
 const policecheck = document.getElementById('police');
 policecheck.addEventListener('change', (event) =>{
     if (event.target.checked){
+        policeLayer.addTo(myMap);
         d3.json(police).then(function(data_po) {
             console.log(data_po);
             for (var i = 0; i < data_po.length; i++) {
                 var loc = [data_po[i].Y, data_po[i].X];
                 if (loc) {
-                    var marker = L.marker(loc, {icon: blueMarker}).addTo(myMap);
+                    marker = L.marker(loc, {icon: blueMarker}).addTo(policeLayer);
                 }
                 marker.bindPopup("<h1>" + data_po[i].NAME + "</h1> <hr> <h4>" + data_po[i].CONTACT + "</h4>");
             };
+            policeLayer.addTo(myMap);
         });
     }
     else{
-        loc = [];
-        var marker = L.marker(loc, {icon: blueMarker}).addTo(myMap);
-        marker.bindPopup("<h1>" + data_po[i].NAME + "</h1> <hr> <h4>" + data_po[i].CONTACT + "</h4>");
+        myMap.removeLayer(policeLayer);
     }
 });
 
@@ -160,11 +164,15 @@ grocerycheck.addEventListener('change', (event) =>{
             for (var i = 0; i < data_g.length; i++) {
                 var loc = [data_g[i].Y, data_g[i].X];
                 if (loc) {
-                    var marker = L.marker(loc, {icon: greenMarker}).addTo(myMap);
+                    marker = L.marker(loc, {icon: greenMarker}).addTo(groceryLayer);
                 }
                 marker.bindPopup("<h1>" + data_g[i].STORENAME + "</h1> <hr> <h4>" + data_g[i].ADDRESS + "</h4>");
             };
+            groceryLayer.addTo(myMap);
         });
+    }
+    else{
+        myMap.removeLayer(groceryLayer);
     }
 });
 
@@ -176,11 +184,15 @@ pubschoolcheck.addEventListener('change', (event) =>{
             for (var i = 0; i < data_pu.length; i++) {
                 var loc = [data_pu[i].Y, data_pu[i].X];
                 if (loc) {
-                    var marker = L.marker(loc, {icon: purpleMarker}).addTo(myMap);
+                    marker = L.marker(loc, {icon: purpleMarker}).addTo(publicLayer);
                 }
                 marker.bindPopup("<h1>" + data_pu[i].NAME + "</h1> <hr> <h4>" + data_pu[i].ADDRESS + "</h4>");
             };
+            publicLayer.addTo(myMap);
         });
+    }
+    else{
+        myMap.removeLayer(publicLayer);
     }
 });
 
@@ -192,10 +204,15 @@ prischoolcheck.addEventListener('change', (event) =>{
             for (var i = 0; i < data_pr.length; i++) {
                 var loc = [data_pr[i].Y, data_pr[i].X];
                 if (loc) {
-                    var marker = L.marker(loc, {icon: dapurpleMarker}).addTo(myMap);
+                    marker = L.marker(loc, {icon: dapurpleMarker}).addTo(privateLayer);
                 }
                 marker.bindPopup("<h1>" + data_pr[i].NAME + "</h1> <hr> <h4>" + data_pr[i].ADDRESS + "</h4>");
             };
+            privateLayer.addTo(myMap);
         });
     }
+    else{
+        myMap.removeLayer(privateLayer);
+    }
 });
+
